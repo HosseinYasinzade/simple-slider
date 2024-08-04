dd6a22fc685e2986b734ae9c1ca2de0f087e5cb3
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-08-04 14:39:56 +0330

    end site brightnes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // variable
 const body = document.querySelector("body");
 const brightnesBtn = document.querySelector(".brightnes");
+const backgroundChange = document.querySelector(".bg");
 
 // flag var
 let isBrightnesCheck = 0;
@@ -11,9 +12,13 @@ let isBrightnesCheck = 0;
 brightnesBtn.addEventListener("click", function () {
   if (isBrightnesCheck === 0) {
     body.style.backgroundColor = "#303030";
+    brightnesBtn.textContent = "Light Mode";
+    backgroundChange.style.backgroundColor = "blue";
     isBrightnesCheck = 1;
   } else {
     body.style.backgroundColor = "lightcyan";
+    brightnesBtn.textContent = "Dark Mode";
+    backgroundChange.style.backgroundColor = "blueviolet";
     isBrightnesCheck = 0;
   }
 });
